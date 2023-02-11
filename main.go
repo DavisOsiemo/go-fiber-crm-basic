8fd8a90a43fd09f74b6ed96d88e11f9d5ac74e7d
Davis Osiemo <davis.nyagwachi@africastalking.com>
2023-02-11 22:29:53 +0300

    Initial commit

---
+++ main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"github.com/davis/go-fiber-crm-basic/database"
+	"github.com/davis/go-fiber-crm-basic/lead"
+	"github.com/gofiber/fiber"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/sqlite"
+	
+)
+
+func setupRoutes(app *fiber.App){
+	app.Get("/api/v1/lead", lead.GetLeads)
+	app.Get("/api/v1/lead/:id", lead.GetLead)
+	app.Post("/api/v1/lead", lead.NewLead)
+	app.Delete("/api/v1/lead/:id", lead.DeleteLead)
+}
+
+func initDatabase(){
+	var err error
+	database.DBConn, err = gorm.Open("sqlite3", "leads.db")
+	if err != nil {
+		panic("Failed to connect to database")
+	}
+	fmt.Println("Connection added to database")
+	database.DBConn.AutoMigrate(&lead.Lead{})
+	fmt.Println("Database migrated")
+}
+
+func main(){
+	app := fiber.New()
+	initDatabase()
+	setupRoutes(app)
+	app.Listen(3000)
+	defer database.DBConn.Close()
+}
+
+{
+	"name": "Davis Osiemo",
+	"company": "Drinks Zetu",
+	"email": "davisossy@gmail.com"
+	"phone": "+254729135212"
+
+}
